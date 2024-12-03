b10b1bb01995b4c5275049f55c4369d3a5bb7f88
soyeon <soyeon20031010@gmail.com>
2024-12-03 10:46:51 +0900

    rename: login component style 분리

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import * as S from "@styles/Login/Login";
 import View from "@assets/image/View_light.svg";
 import ViewHide from "@assets/image/View_hide_light.svg";
 
@@ -46,116 +46,22 @@ const Login = () => {
 
     return(
         <>
-            <LDiv>
-                <LText>관리자 로그인</LText>
-                <InputDiv>
-                    <LID onChange={(e)=>setInputID(e.target.value)} isIn={inputID!=""}/>
-                    <PWDiv>
-                        <LPW onChange={(e)=>setInputPW(e.target.value)} isIn={inputPW!=""} type={showPW?'text':'password'}/>
+            <S.LDiv>
+                <S.LText>관리자 로그인</S.LText>
+                <S.InputDiv>
+                    <S.LID onChange={(e)=>setInputID(e.target.value)} isIn={inputID!=""}/>
+                    <S.PWDiv>
+                        <S.LPW onChange={(e)=>setInputPW(e.target.value)} isIn={inputPW!=""} type={showPW?'text':'password'}/>
                         {inputPW && (
-                            showPW?<PWBtn src={View} onClick={()=>setShowPW(!showPW)}/>:<PWBtn src={ViewHide} onClick={()=>setShowPW(!showPW)}/>
+                            showPW?<S.PWBtn src={View} onClick={()=>setShowPW(!showPW)}/>:<S.PWBtn src={ViewHide} onClick={()=>setShowPW(!showPW)}/>
                         )}
-                    </PWDiv>
-                </InputDiv>
-                {isCompare? <></> : <ErrDiv><LError>*아이디 또는 비밀번호가 일치하지 않습니다. 다시 시도해주세요.*</LError></ErrDiv>}
-                <LBtn isClick={inputID!=""&&inputPW!=""} onClick={handleLogin}>로그인</LBtn>
-            </LDiv>
+                    </S.PWDiv>
+                </S.InputDiv>
+                {isCompare? <></> : <S.ErrDiv><S.LError>*아이디 또는 비밀번호가 일치하지 않습니다. 다시 시도해주세요.*</S.LError></S.ErrDiv>}
+                <S.LBtn isClick={inputID!=""&&inputPW!=""} onClick={handleLogin}>로그인</S.LBtn>
+            </S.LDiv>
         </>
     );
 };
 export default Login;
 
-const LDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100%;
-`;
-const LText = styled.div`
-    display: flex;
-    color: var(--Black);
-    text-align: center;
-    font: var(--PC_LoginText);
-`;
-
-const InputDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 400px;
-    margin: 60px 0 60px 0;
-`;
-
-const LID= styled.input.attrs({
-    placeholder: "아이디를 입력하세요"
-})<{isIn:boolean}>`
-    display: flex;
-    height: 60px;
-    padding: 0px 20px;
-    border-radius: 4px;
-    background: ${({isIn})=>(isIn)?`White`:`var(--M5)`};
-    border-style: none;
-    border: ${({isIn})=>(isIn)?`1px solid var(--M30)`:``};
-    outline: none;
-    text-justify: center;
-    line-height: 80px;
-    font: var(--PC_InputText);
-    align-items:center;
-    justify-content: center;
-    &::placeholder {
-        color: var(--M30);
-        font: var(--PC_InputText);
-    }
-`;
-
-const PWDiv = styled.div`
-    display: flex;
-    width: 100%;
-    margin-top: 20px;
-`;
-
-const LPW = styled(LID).attrs({
-    placeholder: "비밀번호를 입력하세요"
-})`
-    width: 100%;
-    background-img: url('@assets/image/View_light.svg');
-`;
-
-const PWBtn = styled.img`
-    display: flex;
-    cursor: pointer;
-    position: absolute;
-    transform: translate(350px,50%);
-    width: 32px;
-    height: 32px;
-`;
-
-const LBtn = styled.button<{isClick: boolean}>`
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    width: 400px;
-    height: 60px;
-    border-radius: 4px;
-    background: ${({ isClick }) => (isClick ? `var(--Primary)` : `var(--M50)`)};
-    color: var(--White);
-    font: var(--PC_ButtonText);
-    cursor: pointer;
-    border:none;
-`;
-
-const ErrDiv = styled.div`
-    display: flex;
-    width: auto;
-    justify-content: center;
-`;
-
-const LError = styled.p`
-    display: flex;
-    color: var(--Secondary);
-    font: var(--PC_BodyText);
-    margin: 0 0 60px 0;
-`;
