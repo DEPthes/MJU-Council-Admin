4ecae03aac78a690b28fa57077df333c899b2885
si14444 <a1@1ui-MacBookAir.local>
2024-12-20 15:19:16 +0900

    fix: 모바일 레이아웃 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,9 @@
 import { parseUrlToArray } from "@/utils/parseUrlToArray";
 import * as S from "@styles/Layout";
+import { useLocation } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import TextBanner from "./common/TextBanner";
-import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -14,7 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   // 정규 표현식을 사용하여 URL의 마지막에 숫자가 있는지 확인
   const isNumberAtEnd = /\/(\d+)\/?$/.test(location.pathname);
 
-  const isLoginPage = location.pathname === "/login";
+  const isLoginPage = location.pathname === "/";
 
   const urlList = parseUrlToArray(location.pathname);
   return (
@@ -22,7 +22,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <NavBar />
       <TextBanner urlList={urlList} isShow={!isNumberAtEnd} />
       <S.Main>{children}</S.Main>
-      {!isLoginPage ?<Footer />:undefined}
+      {!isLoginPage ? <Footer /> : undefined}
     </S.Container>
   );
 };
--- src/styles/Login/Login.ts
+++ src/styles/Login/Login.ts
@@ -1,115 +1,111 @@
 import styled from "styled-components";
 
 export const LDiv = styled.div`
-position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-    margin: auto;
-    background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: white;
 `;
 export const LText = styled.div`
-    display: flex;
-    color: var(--Black);
-    text-align: center;
-    font: var(--PC_LoginText);
+  display: flex;
+  color: var(--Black);
+  text-align: center;
+  font: var(--PC_LoginText);
 `;
 
 export const InputDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 400px;
-    margin: 60px 0 60px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 400px;
+  margin: 60px 0 60px 0;
 `;
 
-export const LID= styled.input<{$isid:string}>`
-    display: flex;
-    height: 60px;
-    padding: 0px 20px;
-    border-radius: 4px;
-    background: ${({$isid})=>($isid=="true")?`White`:`var(--M5)`};
-    border-style: none;
-    border: ${({$isid})=>($isid=="true")?`1px solid var(--M30)`:``};
-    outline: none;
-    text-justify: center;
-    line-height: 80px;
+export const LID = styled.input<{ $isid: string }>`
+  display: flex;
+  height: 60px;
+  padding: 0px 20px;
+  border-radius: 4px;
+  background: ${({ $isid }) => ($isid == "true" ? `White` : `var(--M5)`)};
+  border-style: none;
+  border: ${({ $isid }) => ($isid == "true" ? `1px solid var(--M30)` : ``)};
+  outline: none;
+  text-justify: center;
+  line-height: 80px;
+  font: var(--PC_InputText);
+  align-items: center;
+  justify-content: center;
+  &::placeholder {
+    color: var(--M30);
     font: var(--PC_InputText);
-    align-items:center;
-    justify-content: center;
-    &::placeholder {
-        color: var(--M30);
-        font: var(--PC_InputText);
-    }
+  }
 `;
 
 export const PWDiv = styled.div`
-    display: flex;
-    width: 100%;
-    margin-top: 20px;
+  display: flex;
+  width: 100%;
+  margin-top: 20px;
 `;
 
-export const LPW = styled.input<{$ispw:string}>`
-    display: flex;
-    height: 60px;
-    padding: 0px 20px;
-    border-radius: 4px;
-    background: ${({$ispw})=>($ispw=="true")?`White`:`var(--M5)`};
-    border-style: none;
-    border: ${({$ispw})=>($ispw=="true")?`1px solid var(--M30)`:``};
-    outline: none;
-    text-justify: center;
-    line-height: 80px;
+export const LPW = styled.input<{ $ispw: string }>`
+  display: flex;
+  height: 60px;
+  padding: 0px 20px;
+  border-radius: 4px;
+  background: ${({ $ispw }) => ($ispw == "true" ? `White` : `var(--M5)`)};
+  border-style: none;
+  border: ${({ $ispw }) => ($ispw == "true" ? `1px solid var(--M30)` : ``)};
+  outline: none;
+  text-justify: center;
+  line-height: 80px;
+  font: var(--PC_InputText);
+  align-items: center;
+  justify-content: center;
+  &::placeholder {
+    color: var(--M30);
     font: var(--PC_InputText);
-    align-items:center;
-    justify-content: center;
-    &::placeholder {
-        color: var(--M30);
-        font: var(--PC_InputText);
-    }
-    width: 100%;
-    background-img: url('@assets/image/View_light.svg');
+  }
+  width: 100%;
+  background-img: url("@assets/image/View_light.svg");
 `;
 
 export const PWBtn = styled.img`
-    display: flex;
-    cursor: pointer;
-    position: absolute;
-    transform: translate(350px,50%);
-    width: 32px;
-    height: 32px;
+  display: flex;
+  cursor: pointer;
+  position: absolute;
+  transform: translate(350px, 50%);
+  width: 32px;
+  height: 32px;
 `;
 
-export const LBtn = styled.button<{$isclick: string}>`
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    width: 400px;
-    height: 60px;
-    border-radius: 4px;
-    background: ${({ $isclick }) => ($isclick=="true" ? `var(--Primary)` : `var(--M50)`)};
-    color: var(--White);
-    font: var(--PC_ButtonText);
-    cursor: pointer;
-    border:none;
+export const LBtn = styled.button<{ $isclick: string }>`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 60px;
+  border-radius: 4px;
+  background: ${({ $isclick }) =>
+    $isclick == "true" ? `var(--Primary)` : `var(--M50)`};
+  color: var(--White);
+  font: var(--PC_ButtonText);
+  cursor: pointer;
+  border: none;
 `;
 
 export const ErrDiv = styled.div`
-    display: flex;
-    width: auto;
-    justify-content: center;
+  display: flex;
+  width: auto;
+  justify-content: center;
 `;
 
 export const LError = styled.p`
-    display: flex;
-    color: var(--Secondary);
-    font: var(--PC_BodyText);
-    margin: 0 0 60px 0;
+  display: flex;
+  color: var(--Secondary);
+  font: var(--PC_BodyText);
+  margin: 0 0 60px 0;
 `;
